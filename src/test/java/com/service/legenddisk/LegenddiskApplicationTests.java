e9f2b033516d044fab1d3d5772aa73d9f5343159
qq610398531 <610398531@qq.com>
2019-11-21 17:39:15 +0800

    Initial commit

---
+++ src/test/java/com/service/legenddisk/LegenddiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.legenddisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegenddiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
